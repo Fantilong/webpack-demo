d3ea5e7465a3a85ef1aa1478873d9b2a55b88bd8
fantl <fantl@shanhs.com>
2022-06-26 11:50:58 +0800

    feat: 使用 多入口配置 dependOn 和 shared 实现公用代码抽离

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import _ from "lodash";
-import "./style.css";
-import weback from "./webpack.png";
+import printMe from "./print";
+
 
 function component() {
     var element = document.createElement('div')
-    element.innerHTML = _.join(['Hello', 'Webpack'], ' ')
-    element.setAttribute('class', 'hello')
 
+    element.innerHTML = _.join(['Hello', 'Webpack'], ' ')
+    
     return element
 }
 
+function componentBtn() {
+    var btn = document.createElement('button')
+
+    btn.innerHTML = 'Click me and check the console'
+    btn.onclick = printMe
+    return btn
+}
+
 document.body.appendChild(component())
+document.body.appendChild(componentBtn())
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack')
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared'
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared'
+        },
+        shared: 'lodash'
+    },
+    // devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({title: 'Code spliting'}),
+        // new webpack.optimize.Commons
+    ],
+    optimization: {
+        runtimeChunk: 'single',
     },
 }
